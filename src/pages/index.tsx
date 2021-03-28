52de7e0b3fc8fe95a6e057ddf9d4cd5118882976
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2021-03-28 15:22:19 -0300

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,7 @@ export default function Home({ posts }: PostsProps) {
       <main className={styles.postsContainer}>
         <h1>Notícias Recentes</h1>
         <div className={styles.postsContent}>
+          a
           {posts.map(post => (
             <Link key={post.slug} href={`/posts/${post.slug}`}>
               <a>
@@ -72,8 +73,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
       })
     }
   })
-
-  console.log(posts)
 
   return {
     props: {
